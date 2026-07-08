37b662e8a5bb5b72bc112369170841e6212ada92
jujulioed <julioeduardon9@hotmail.com>
2026-07-07 21:53:01 -0300

    [frontend] Implent the main screen and the card grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css'
+import { Card } from './components/card/card';
+import { useFoodData } from './hooks/useFoodData';
+import type { FoodData } from './interface/FoodData';
 
 function App() {
+  const { data } = useFoodData();
 
   return (
-    <div className="App">
-      
+    <div className="container">
+      <h1>Cardápio</h1>
+      <div className='card-grid'>
+        {data?.map(foodData => 
+          <Card
+            price={foodData.price}
+            title={foodData.title}
+            image={foodData.image}/>
+        )}
+      </div>
     </div>
   )
-}
+} 
 
 export default App
